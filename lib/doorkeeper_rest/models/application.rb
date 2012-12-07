28af41617ddeee2d34f054a7ca0116a130e45ae8
Omar Qureshi <omar@omarqureshi.net>
2012-12-07 23:09:36 +0100

    filter applications

--- lib/doorkeeper_rest/models/application.rb
+++ lib/doorkeeper_rest/models/application.rb
@@ -1,10 +1,11 @@
 module Doorkeeper
   class Application
-
     def self.by_uid(uid)
       first(:conditions => {:uid => uid})
     end
 
+    def self.authenticate(uid, secret)
+      all({:uid => uid, :secret => secret}).first
+    end
   end
-  
 end
